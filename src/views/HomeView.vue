09561ff146dc8219aaa51908afcab6124718110e
shijiev <2424782003@qq.com>
2023-06-06 21:07:34 +0800

    更换主页logo图片和欢迎语

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="my logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to my Generic Views"/>
   </div>
 </template>
 
